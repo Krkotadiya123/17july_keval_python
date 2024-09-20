d252daf73883bb0dd9e68d00e9e8922a5e6bc5cf
keval <167281609+Krkotadiya123@users.noreply.github.com>
2024-09-20 09:40:19 +0530

    Update Q11lisrt.py

--- assignments/module-3/Q11lisrt.py
+++ assignments/module-3/Q11lisrt.py
@@ -1,7 +1,7 @@
 # Write a Python program to write a list to a file. 
 
 
-list=['Rajkot','Diu','Goa','Jamanagar','Bhavnagar','Dwarka','Somanath']
+list=['Rajkot','dhrol','falla','Jamanagar','Bhavnagar','Dwarka','Somanath']
 
 
 fl=open('kev.txt','w')
